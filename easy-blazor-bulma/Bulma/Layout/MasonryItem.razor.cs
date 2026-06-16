d9c05717d4daf0b73011a264e8f49895cd22debf
Jun Cai <jun@nfsoftware.ca>
2026-06-16 16:22:27 -0400

    Add MasonryItem Blazor wrapper component for masonry-item elements

---
+++ easy-blazor-bulma/Bulma/Layout/MasonryItem.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace easy_blazor_bulma;
+
+/// <summary>
+/// Wraps content as a Masonry layout item.
+/// </summary>
+public partial class MasonryItem : ComponentBase
+{
+	/// <summary>
+	/// Content rendered inside the Masonry item.
+	/// </summary>
+	[Parameter]
+	public RenderFragment? ChildContent { get; set; }
+
+	/// <summary>
+	/// Additional attributes applied to the root item element.
+	/// </summary>
+	[Parameter(CaptureUnmatchedValues = true)]
+	public Dictionary<string, object>? AdditionalAttributes { get; set; }
+
+	private readonly string[] Filter = ["class"];
+	private string MainCssClass => string.Join(' ', "masonry-item", AdditionalAttributes.GetValue("class"));
+}
