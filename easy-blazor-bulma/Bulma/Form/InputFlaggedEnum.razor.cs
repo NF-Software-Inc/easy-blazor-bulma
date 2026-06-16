8813f578533c8380a848869b9048f58099a6240a
Nathanael Frey <nf@nathanaelfrey.ca>
2026-06-16 11:20:10 -0400

    Documentation cleanup and LINQ update

--- easy-blazor-bulma/Bulma/Form/InputFlaggedEnum.razor.cs
+++ easy-blazor-bulma/Bulma/Form/InputFlaggedEnum.razor.cs
@@ -15,17 +15,17 @@ namespace easy_blazor_bulma;
 /// </remarks>
 public partial class InputFlaggedEnum<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] TEnum> : InputBase<TEnum>
 {
-    /// <summary>
-    /// Wraps each item in the .box CSS class when true.
-    /// </summary>
-    [Parameter]
-    public bool IsBoxed { get; set; } = true;
+	/// <summary>
+	/// Wraps each item in the .box CSS class when true.
+	/// </summary>
+	[Parameter]
+	public bool IsBoxed { get; set; } = true;
 
-    /// <summary>
-    /// Specifies whether to hide the option assigned to 0.
-    /// </summary>
-    [Parameter]
-    public bool HideZeroOption { get; set; } = true;
+	/// <summary>
+	/// Specifies whether to hide the option assigned to 0.
+	/// </summary>
+	[Parameter]
+	public bool HideZeroOption { get; set; } = true;
 
 	private readonly string[] Filter = new[] { "class", "switch-class", "label-class" };
 
@@ -34,21 +34,21 @@ public partial class InputFlaggedEnum<[DynamicallyAccessedMembers(DynamicallyAcc
 	private readonly string PropertyName = Guid.NewGuid().ToString("N");
 	private long[] EnumValues = [];
 
-    private string MainCssClass
-    {
-        get
-        {
-            var css = "checkbox";
+	private string MainCssClass
+	{
+		get
+		{
+			var css = "checkbox";
 
-            if (IsBoxed)
-                css += " box";
+			if (IsBoxed)
+				css += " box";
 
 			return string.Join(' ', css, CssClass);
-        }
+		}
 	}
 
-    private string SwitchCssClass => string.Join(' ', "switch", AdditionalAttributes.GetValue("switch-class"));
-    private string LabelCssClass => string.Join(' ', "is-unselectable", AdditionalAttributes.GetValue("label-class"));
+	private string SwitchCssClass => string.Join(' ', "switch", AdditionalAttributes.GetValue("switch-class"));
+	private string LabelCssClass => string.Join(' ', "is-unselectable", AdditionalAttributes.GetValue("label-class"));
 
 	/// <inheritdoc/>
 	protected override void OnInitialized()
@@ -60,91 +60,83 @@ public partial class InputFlaggedEnum<[DynamicallyAccessedMembers(DynamicallyAcc
 		else if (Enum.GetUnderlyingType(UnderlyingType) == typeof(ulong))
 			throw new InvalidOperationException($"Unsupported type param '{UnderlyingType.Name}'. Does not support enums based on ulong.");
 
-        // Populate EnumValues with all non-zero enum values converted to long
-		var rawValues = Enum.GetValues(UnderlyingType);
-		var longValues = new List<long>(rawValues.Length);
-
-		foreach (var v in rawValues)
-		{
-			long l = Convert.ToInt64(v);
-
-            // if the value is zero and HideZeroOption is true, skip it
-            if (l == 0 && HideZeroOption)
-                continue;
-
-            longValues.Add(l);
-		}
-
-		EnumValues = longValues.ToArray();
+		EnumValues = Enum.GetValues(UnderlyingType)
+			.Cast<TEnum>()
+			.Where(x => HideZeroOption == false || (int)(object)x! > 0)
+			.Select(x => Convert.ToInt64(x))
+			.ToArray();
 	}
 
-    /// <inheritdoc/>
-    protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out TEnum result, [NotNullWhen(false)] out string? validationErrorMessage)
-    {
-        if (IsNullable == false && string.IsNullOrWhiteSpace(value))
-        {
-            result = default!;
+	/// <inheritdoc/>
+	protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out TEnum result, [NotNullWhen(false)] out string? validationErrorMessage)
+	{
+		if (IsNullable == false && string.IsNullOrWhiteSpace(value))
+		{
+			result = default!;
 
-            validationErrorMessage = null;
-            return true;
-        }
-        else if (Enum.TryParse(UnderlyingType, value, true, out object? parsed))
-        {
-            result = (TEnum)parsed!;
+			validationErrorMessage = null;
+			return true;
+		}
+		else if (Enum.TryParse(UnderlyingType, value, true, out object? parsed))
+		{
+			result = (TEnum)parsed!;
 
-            validationErrorMessage = null;
-            return true;
-        }
-        else
-        {
-            result = default;
+			validationErrorMessage = null;
+			return true;
+		}
+		else
+		{
+			result = default;
 
-            validationErrorMessage = string.Format(CultureInfo.InvariantCulture, "The {0} field could not be parsed.", DisplayName ?? FieldIdentifier.FieldName);
-            return false;
-        }
-    }
+			validationErrorMessage = string.Format(CultureInfo.InvariantCulture, "The {0} field could not be parsed.", DisplayName ?? FieldIdentifier.FieldName);
+			return false;
+		}
+	}
 
-    /// <inheritdoc />
-    protected override string FormatValueAsString(TEnum? value) => value switch
-    {
-        TEnum enumValue => enumValue.ToString() ?? string.Empty,
-        _ => string.Empty
-    };
+	/// <inheritdoc />
+	protected override string FormatValueAsString(TEnum? value) => value switch
+	{
+		TEnum enumValue => enumValue.ToString() ?? string.Empty,
+		_ => string.Empty
+	};
 
-    private void OnValueChanged(TEnum flag)
-    {
-        if (AdditionalAttributes.IsDisabled())
-            return;
+	/// <summary>
+	/// Handles the change event for each checkbox, updating the current value by setting or unsetting the corresponding flag.
+	/// If a flag is being unset, any flags that are dependent on it (i.e., flags that have the bit represented by the flag being toggled) will also be unset.
+	/// </summary>
+	/// <param name="flag">The flag that was toggled.</param>
+	private void OnValueChanged(TEnum flag)
+	{
+		if (AdditionalAttributes.IsDisabled())
+			return;
 
-        long current = CurrentValue != null ? Convert.ToInt64(CurrentValue) : 0L;
-        long update = Convert.ToInt64(flag);
+		long current = CurrentValue != null ? Convert.ToInt64(CurrentValue) : 0L;
+		long update = Convert.ToInt64(flag);
 
-        // if the flag is already set, unset it, also unset any flags that are dependent on it
-        if ((current & update) == update)
-        {
-            long removeMask = 0;
+		if ((current & update) == update)
+		{
+			long removeMask = 0;
 
-            foreach (long enumValue in EnumValues)
-            {
-                // if the enum value has the bit represented by update toggled, add it to the remove mask
-                if ((enumValue & update) != 0)
-                    removeMask |= enumValue;
-            }
+			foreach (long enumValue in EnumValues)
+			{
+				if ((enumValue & update) != 0)
+					removeMask |= enumValue;
+			}
 
-            current &= ~removeMask; // unset the bits in the remove mask
-        }
-        else
-        {
-            current |= update;  // set the bit represented by update
-        }
+			current &= ~removeMask;
+		}
+		else
+		{
+			current |= update;
+		}
 
-        CurrentValueAsString = Enum.Parse(UnderlyingType, current.ToString()).ToString();
-    }
+		CurrentValueAsString = Enum.Parse(UnderlyingType, current.ToString()).ToString();
+	}
 
-    private bool IsFlagChecked(TEnum flag)
-    {
-        return CurrentValue != null && (Convert.ToInt64(CurrentValue) & Convert.ToInt64(flag)) == Convert.ToInt64(flag);
-    }
+	private bool IsFlagChecked(TEnum flag)
+	{
+		return CurrentValue != null && (Convert.ToInt64(CurrentValue) & Convert.ToInt64(flag)) == Convert.ToInt64(flag);
+	}
 
-    private string GetEnumSwitchId(TEnum value) => $"switch-InputFlaggedEnum-{PropertyName}-{value}";
+	private string GetEnumSwitchId(TEnum value) => $"switch-InputFlaggedEnum-{PropertyName}-{value}";
 }
