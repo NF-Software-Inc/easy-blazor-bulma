df3af02914cdf3b145168f89b46cb1a8742a12fe
Nathanael Frey <nf@nathanaelfrey.ca>
2026-08-10 09:09:35 -0400

    Add regex matchers to MarkupViewer

--- easy-blazor-bulma/Bulma/Layout/MarkupViewer.razor.cs
+++ easy-blazor-bulma/Bulma/Layout/MarkupViewer.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System.Text.RegularExpressions;
 
 namespace easy_blazor_bulma;
 
@@ -13,11 +14,61 @@ public partial class MarkupViewer : ComponentBase
 	[Parameter]
 	public required string Content { get; set; }
 
+	/// <summary>
+	/// Specifies whether to replace line breaks in the content with <br /> tags.
+	/// </summary>
+	[Parameter]
+	public bool ReplaceLineBreaks { get; set; }
+
+	/// <summary>
+	/// Specifies whether to strip HTML comments from the content.
+	/// </summary>
+	[Parameter]
+	public bool StripComments { get; set; }
+
+	/// <summary>
+	/// Specifies whether to remove script tags from the content.
+	/// </summary>
+	[Parameter]
+	public bool RemoveScripts { get; set; }
+
 	private MarkupString? Display;
 
 	/// <inheritdoc />
-	protected override void OnInitialized()
+	public async override Task SetParametersAsync(ParameterView parameters)
 	{
-		Display = new MarkupString(Content);
+		if (parameters.TryGetValue<string>(nameof(Content), out var updated) && updated != Content)
+		{
+			if (ReplaceLineBreaks)
+				updated = MatchLineBreaks().Replace(updated, "<br />");
+
+			if (StripComments)
+				updated = MatchHtmlComments().Replace(updated, "");
+
+			if (RemoveScripts)
+				updated = MatchScripts().Replace(updated, "");
+
+			Display = new MarkupString(updated);
+		}
+
+		await base.SetParametersAsync(parameters);
 	}
+
+	/// <summary>
+	/// Matches comments in HTML or XML documents.
+	/// </summary>
+	[GeneratedRegex("<!--(.*?)-->", RegexOptions.Multiline, 1_000)]
+	private static partial Regex MatchHtmlComments();
+
+	/// <summary>
+	/// Matches line breaks for Windows or Linux line ending styles.
+	/// </summary>
+	[GeneratedRegex(@"\r?\n|\r", RegexOptions.Multiline, 1_000)]
+	private static partial Regex MatchLineBreaks();
+
+	/// <summary>
+	/// Matches script tags in HTML documents, including their content.
+	/// </summary>
+	[GeneratedRegex(@"<script\b[^>]*>(?:[\s\S]*?</script>)|<script\b[^>]*/>", RegexOptions.IgnoreCase | RegexOptions.Singleline, 1_000)]
+	private static partial Regex MatchScripts();
 }
